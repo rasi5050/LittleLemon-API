4e462d45548f8becd96b8ece3c75cd7732a0e133
rasi <rasi@Rasis-MBP.lan>
2023-10-31 11:06:10 -0400

    configure database

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Menu, Booking
 # Register your models here.
+admin.site.register([Booking, Menu])
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
