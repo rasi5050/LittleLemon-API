8c70ace4fd3b23f98db95cd9979de3829c5dfa6e
rasi <rasi@Rasis-MBP.lan>
2023-10-31 08:45:49 -0400

    added welcome image

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['restaurant/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,11 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'restaurant/static/'
 
 # not specified in the excercise, but found mandatory
 STATICFILES_DIRS = [
-    "static",
+    "restaurant/static",
 ]
 
 # Default primary key field type
